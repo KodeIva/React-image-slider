1f3b43aa9be206b9c0861f3b32622e0f7ddd2342
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-05-03 14:30:34 +0100

    React icons added, tailwind styling

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import {RxDotFilled} from 'react-icons/rx'
 
 function App() {
 
   const images = [
     {
       url: 'https://images.unsplash.com/photo-1602615576820-ea14cf3e476a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      name: 'flowers'
+      name: 'Flowers'
     },
     {
       url: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
@@ -19,44 +20,51 @@ function App() {
 
     {
       url: 'https://images.unsplash.com/photo-1491591462767-3b91b2a19487?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      name: 'yellow flower'
+      name: 'Yellow Flowers'
     },
     {
       url: 'https://images.unsplash.com/photo-1577862236121-c237c73ef4ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      name: 'Spring'
     },
   ];
 
   const[currentImage, setCurrentImage] = useState(0)
 
-  function prevImage() {
-    if(currentImage === 0) {
-      setCurrentImage(images.length - 1)
-    }else{
-      setCurrentImage(currentImage - 1)
-    }
-  }
-
- function nextImage() {
-  if(currentImage === images.length - 1) {
-    setCurrentImage(0)
-  }else{
-    setCurrentImage(currentImage + 1)
-  }
+function prevImage() {
+ if(currentImage === 0) {
+  setCurrentImage(images.length - 1)
+ }else{
+  setCurrentImage(currentImage - 1)
  }
+}
+
+function nextImage() {
+ if(currentImage === images.length - 1) {
+  setCurrentImage(0)
+ }else{
+  setCurrentImage(currentImage + 1)
+ }
+}
 
   return (
     <div className=" w-[100%] h-[100vh] flex flex-col justify-center items-center bg-slate-300">
-     <div className="w-[70%] h-[auto] ">
+     <div className="w-[90%] h-[auto] md:w-[80%] lg:w-[60%]">
       <img className="w-[100%] h-[auto] " src={images[currentImage].url} alt="" />
      </div>
-    <div className='w-[200px] text-center h-6'>
-          <p>{images[currentImage].name}</p>
-    </div>
-     <div>
-      <button className='p-3' type='button' onClick={prevImage}>Prev</button>
-      <button className='p-3' type='button' onClick={nextImage}>next</button>
-     </div>
+
+
  
+     <div className='flex justify-between w-[70%] lg:w-[50%] '>
+      <div className='p-3 ' type='button' onClick={prevImage}>
+         <FiChevronLeft className='text-4xl text-white'/>
+      </div>
+      <div className='w-[200px] text-center h-6 '>
+        <p className='text-white m-4 text-2xl '>{images[currentImage].name}</p>
+      </div>
+      <div className='p-3 ' type='button' onClick={nextImage}>
+        <FiChevronRight className='text-4xl text-white' /></div>
+      </div>
+
     </div>
   );
 }
